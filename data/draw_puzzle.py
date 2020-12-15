a6f8573ca99eeb3f1ba34e73a1b06d0afac0066d
malihanda <handamalaika@gmail.com>
2020-12-15 12:53:59 -0500

    add support for non-square puzzles

--- data/draw_puzzle.py
+++ data/draw_puzzle.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 import numpy as np
 import puz
 import sys
@@ -7,9 +8,9 @@ import sys
 SIZES = {
     "ROWS": None,
     "COLS": None,
-    "CELL_SIZE": 20,
+    "CELL_SIZE": 40,
     "BORDER": 0,
-    "GRID_LINE": 2,
+    "GRID_LINE": 4,
     "PUZZLE_HEIGHT": None,
     "PUZZLE_WIDTH": None, 
 }
@@ -21,14 +22,57 @@ GREY = [150, 150, 150]
 BORDER_COLOR = [80, 80, 80]
 
 
-# Parse the puz file and return a 2d array of Xs and .s
+# Pad an array so that it's square or almost square.
+# This method will only add padding in equal amounts so (ex.) a 15x16 would
+# remain unchanged.
+def pad_array(a):
+    extra = abs(SIZES["ROWS"] - SIZES["COLS"]) // 2
+    if extra == 0:
+        return a
+
+    # Add extra rows
+    if SIZES["ROWS"] < SIZES["COLS"]:
+        row = ["."] * SIZES["COLS"]
+        new = []
+        for _ in range(extra):
+            new.append(row)
+        for e in a:
+            new.append(e)
+        for _ in range(extra):
+            new.append(row)
+
+    # Add extra cols
+    if SIZES["COLS"] < SIZES["ROWS"]:
+        new = []
+        padding = ["."] * extra
+        for e in a:
+            new_row = padding + e + padding
+            new.append(new_row)
+
+    # The size of the puzzle has now changed, so update the dict
+    SIZES["ROWS"] = len(new)
+    SIZES["COLS"] = len(new[0])
+    SIZES["PUZZLE_HEIGHT"] = (
+        (SIZES["BORDER"] * 2) + 
+        (SIZES["CELL_SIZE"] * SIZES["ROWS"]) + 
+        ((SIZES["ROWS"] - 1) * SIZES["GRID_LINE"]))
+    SIZES["PUZZLE_WIDTH"] = (
+        (SIZES["BORDER"] * 2) + 
+        (SIZES["CELL_SIZE"] * SIZES["COLS"]) + 
+        ((SIZES["COLS"] - 1) * SIZES["GRID_LINE"]))
+
+    return new
+
+
+# Parse the puz file and return a 2d near-square array of letters and .s
 def read_puzzle():
     array = [[0] * SIZES["COLS"] for _ in range(SIZES["ROWS"])]
     for i in range(SIZES["ROWS"]):
         for j in range(SIZES["COLS"]):
             array[i][j] = PUZZLE.solution[(i * SIZES["COLS"]) + j]
 
-    return array
+    padded_array = pad_array(array)
+    return padded_array
 
 
 # Draw a single cell of the puzzle
@@ -58,6 +102,28 @@ def draw_puzzle(p):
 
     return image
 
+
+# Ensure the image is square by adding a border if necessary.
+# A 15x (e.g.) puzzle will be unchanged, a 15x16 (e.g.) will have black pixels
+# added to the edges to make the final image square.
+def center_puzzle(p):
+    sh = p.shape
+    if sh[0] == sh[1]:
+        return p
+
+    # Create a square image that will hold the puzzle
+    dim = max(sh)
+    sq = np.zeros((dim, dim, 3), dtype=np.uint8)
+
+    # Insert the puzzle into the square image
+    r_start = (dim - sh[0]) // 2
+    r_stop = r_start + sh[0]
+    c_start = (dim - sh[1]) // 2
+    c_stop = c_start + sh[1]
+    sq[r_start:r_stop, c_start:c_stop] = p
+
+    return sq
+
 if __name__ == "__main__":
     FILE_PATH = sys.argv[1]
     PUZ_FILE = "puz_files/{}.puz".format(FILE_PATH)
@@ -78,5 +144,6 @@ if __name__ == "__main__":
 
     # Compute and save the image
     p = read_puzzle()
-    pic = draw_puzzle(p)
-    cv2.imwrite(IMAGE_FILE, pic)
+    raw = draw_puzzle(p)
+    final = center_puzzle(raw)
+    cv2.imwrite(IMAGE_FILE, final)
